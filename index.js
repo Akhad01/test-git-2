2c11d50cbc431aaf8db779e9811421e6b643253d
Akhad <abdurazakovahad@gmail.com>
2023-08-20 22:07:21 +0300

    made changes in feature/access

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log('Hello world')
 
-console.log('branch develop')
+const age = 21
+
+if (age > 18) {
+  console.log('Вы совершенолетний')
+} else {
+  console.log('Вы не совершенолетний')
+}
